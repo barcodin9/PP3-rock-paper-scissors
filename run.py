3a9907c50a3d68c83d9f947839ad8d73b0ccc011
barcodin9 <barcodingss@gmail.com>
2023-09-12 21:56:03 +0100

    Add player choice function

--- run.py
+++ run.py
@@ -1,2 +1,13 @@
 import random
 
+def player_choice():
+    """Get the player choice - rock, paper or scissors."""
+
+    while True:
+        uChoice = input("Choose rock, paper or scissors: ").lower()
+        if uChoice in ["rock", "paper", "scissors"]:
+            return uChoice
+        else:
+            print("Invalid choice. Choose rock, paper or scissors.")
+
+player_choice()
